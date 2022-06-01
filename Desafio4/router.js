88f8234808f5efcb93b9ec4235c39c3462cf788f
florradzik <flor.r@icloud.com>
2022-06-01 09:27:01 -0300

    Added form

--- Desafio4/router.js
+++ Desafio4/router.js
@@ -1,41 +1,59 @@
 const express = require("express")
-const multer = require("multer")
-
-const { Router } = express
-const app = express()
+const router = express.Router()
 
 const DB = []
+const app = express()
 
 app.use("/static", express.static(__dirname + "/public"))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const router = Router()
+app.get("/", (req, res) => {
+  res.send("server OK")
+})
 
 router.get("/", (req, res) => {
   res.json(DB)
 })
 
 router.get("/:id", (req, res) => {
-  const id = req.params.id
-  res.json(DB.find((d) => d.id == id))
+  const id = Number(req.params.id)
+  const obj = DB.find((d) => d.id === id)
+  if (obj == undefined) {
+    res.send({ error: "Producto no encontrado" })
+  } else {
+    res.json(obj)
+  }
 })
 
 router.post("/", (req, res) => {
-  DB.push(req.body)
+  const id = getLastID()
+  DB.push({
+    ...req.body,
+    ...{ id: id + 1 },
+  })
   res.send("Object pushed")
 })
 
+const getLastID = () => {
+  const l = DB.length
+
+  if (l < 1) return 0
+
+  return DB[DB.length - 1].id
+}
+
 router.put("/:id", (req, res) => {
   const id = req.params.id
-  objIndex = DB.findIndex((obj) => obj.id == id)
+  objIndex = DB.findIndex((obj) => obj.id === id)
   DB[objIndex] = req.body
 })
 
 router.delete("/:id", (req, res) => {
   const id = req.params.id
-  DB = DB.map((d) => d.id != id)
+  const idx = DB.findIndex((p) => p.id === id)
+  DB.splice(idx, 1)
 })
 
 app.use("/api/products", router)
