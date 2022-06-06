0a6346953a09c7874d44b01a85a3d7cf42d77f7b
florradzik <flor.r@icloud.com>
2022-06-06 10:36:43 -0300

    added templates and routes

---
+++ Desafio5/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const { Router } = express
+const app = express()
+const router = Router()
+const productos = []
+
+app.set("views", "./views")
+app.set("view engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Espacio donde se encuentra el formulario
+app.get("/", (req, res) => {
+  res.render("index")
+})
+
+// Espacio donde se encuentran todos los productos
+router.post("/", (req, res) => {
+  productos.push(req.body)
+  console.log(req.body, "added to personas")
+  res.render("table", { personas })
+})
+
+app.use("/api/products", router)
+
+app.listen(8080)
+
+console.log("Running...")
